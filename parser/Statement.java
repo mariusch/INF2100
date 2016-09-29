352f9961ec1bb50ce0d3d58101f45423e8e88fa6
Marius Christensen <marius93@gmail.com>
2016-09-29 11:44:45 +0200

    Statement klassen

--- parser/Statement.java
+++ parser/Statement.java
@@ -1,11 +1,10 @@
 package parser;
 
-import main.*;
 import scanner.*;
 import static scanner.TokenKind.*;
 
-abstract class Statement extends PascalSyntax {
 
+abstract class Statement extends PascalSyntax {
 
     Statement(int lNum) {
         super(lNum);
@@ -16,13 +15,36 @@ abstract class Statement extends PascalSyntax {
         return "<NAVN-HER> on line " + lineNum;
     }
 
+
     static Statement parse(Scanner s) {
-        enterParser("while-statm");
-
-        //Statement stm = new Statement(s.curToken.id, s.curLineNum());
-
-        leaveParser("while-statm");
-        return null;
+        enterParser("statement");
+        Statement st = null;
+        switch (s.curToken.kind) {
+            case beginToken:
+                st = CompoundStatm.parse(s);
+                break;
+            case ifToken:
+                st = IfStatm.parse(s);
+                break;
+            case nameToken:
+                switch (s.nextToken.kind) {
+                    case assignToken:
+                    case leftBracketToken:
+                        st = AssignStatm.parse(s);
+                        break;
+                    default:
+                        st = ProcCallStatm.parse(s);
+                        break;
+                }
+                break;
+            case whileToken:
+                st = WhileStatm.parse(s);
+                break;
+            default:
+                st = EmptyStatm.parse(s);
+                break;
+        }
+        leaveParser("statement");
+        return st;
     }
-
 }
