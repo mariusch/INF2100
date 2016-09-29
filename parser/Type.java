dd886cdcbfb59805e3398dfcb85b94c1a9a954d5
silmer <dahl.silmer@gmail.com>
2016-09-29 13:26:02 +0200

    Endret i Type

--- parser/Type.java
+++ parser/Type.java
@@ -1,7 +1,13 @@
 package parser;
 
+import scanner.Scanner;
+
 /**
  * Created by essi on 29.09.2016.
  */
-public class Type {
+public class Type extends  {
+    public static Type parse(Scanner s) {
+    }
 }
+
+//Skal vi egentlig ha denne klassen, eller kun den i types?
