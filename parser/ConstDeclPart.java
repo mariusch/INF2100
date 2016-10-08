3945db7b40e5edbf84292ec249eb27feaf875dfd
silmer <dahl.silmer@gmail.com>
2016-10-09 00:23:39 +0200

    Lagt til stuff i ConstDeclPart

--- parser/ConstDeclPart.java
+++ parser/ConstDeclPart.java
@@ -2,6 +2,9 @@ package parser;
 
 import main.*;
 import scanner.*;
+
+import java.util.ArrayList;
+
 import static scanner.TokenKind.*;
 
 class ConstDeclPart extends PascalSyntax {
@@ -10,6 +13,7 @@ class ConstDeclPart extends PascalSyntax {
     ConstDeclPart(int lNum) {
         super(lNum);
     }
+    private ArrayList <ConstDecl> cdList = new ArrayList<>();
 
     @Override
     public String identify() {
@@ -18,15 +22,30 @@ class ConstDeclPart extends PascalSyntax {
 
     @Override
     void prettyPrint() {
+        Main.log.prettyPrint("const ");
+        for (ConstDecl cd : cdList){
+            cd.prettyPrint();
+        }
+
 
     }
 
 
     static ConstDeclPart parse(Scanner s) {
-        enterParser("const decl part");
+        enterParser("const-decl-part");
+
         ConstDeclPart cdp = new ConstDeclPart(s.curLineNum());
 
-        leaveParser("const decl part");
+        s.skip(TokenKind.constToken);
+
+        cdp.cdList.add(ConstDecl.parse(s));
+
+        while (s.curToken.kind == nameToken){
+            cdp.cdList.add(ConstDecl.parse(s));
+        }
+
+
+        leaveParser("const-decl-part");
         return cdp;
     }
 
