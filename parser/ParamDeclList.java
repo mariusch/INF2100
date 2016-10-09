c906ed354f5e0d0dcbde416b405317ce3efcd244
silmer <dahl.silmer@gmail.com>
2016-10-09 11:25:33 +0200

    Fikset ting i ParamDeclList

--- parser/ParamDeclList.java
+++ parser/ParamDeclList.java
@@ -1,11 +1,21 @@
 package parser;
 
 import scanner.Scanner;
+import scanner.TokenKind;
+
+import java.util.ArrayList;
 
 /**
  * Created by marius on 22.09.2016.
+ *
+ * Må inneholde
+    * Venstre parentes
+    * Minst et param decl
+    * Kan inneholde flere param decl såfremt det er et semikolon mellom dem
  */
 class ParamDeclList extends PascalSyntax {
+
+    private ArrayList<ParamDecl> pdList = new ArrayList<>();
 
     ParamDeclList(int lNum) {
         super(lNum);
@@ -23,10 +33,20 @@ class ParamDeclList extends PascalSyntax {
 
     static ParamDeclList parse(Scanner s) {
         enterParser("while-statm");
-        ParamDeclList stm = new ParamDeclList(s.curLineNum());
+        ParamDeclList pdl = new ParamDeclList(s.curLineNum());
+
+        s.skip(TokenKind.leftParToken);
+        pdl.pdList.add(ParamDecl.parse(s));
+
+        //Hvis semikolon skal vi se etter flere param decl
+        while(s.curToken.kind == TokenKind.semicolonToken){
+            pdl.pdList.add(ParamDecl.parse(s));
+        }
+
+        s.skip(TokenKind.rightParToken);
 
         leaveParser("while-statm");
-        return stm;
+        return pdl;
     }
 
 }
