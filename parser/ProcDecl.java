d2e827f818865b709e145b803d5985d786213a94
silmer <dahl.silmer@gmail.com>
2016-10-09 11:12:18 +0200

    Fylt ut ProcDecl
    .parse() og .prettyPrint () (ikke tatt høyde for indent foreløpig)

--- parser/ProcDecl.java
+++ parser/ProcDecl.java
@@ -1,11 +1,17 @@
 package parser;
 
+import main.Main;
 import scanner.Scanner;
+import scanner.TokenKind;
 
 /**
  * Created by marius on 22.09.2016.
  */
 class ProcDecl extends PascalDecl {
+
+    private String name;
+    private ParamDeclList pdl;
+    private Block block;
 
     ProcDecl(String id, int lNum) {
         super(id, lNum);
@@ -18,14 +24,36 @@ class ProcDecl extends PascalDecl {
 
     @Override
     void prettyPrint() {
+        Main.log.prettyPrint("procedure "); Main.log.prettyPrint(name);
+        if (pdl != null)
+            pdl.prettyPrint();
 
+        Main.log.prettyPrint(";");
+        block.prettyPrint();
+        Main.log.prettyPrint(";");
     }
 
     static ProcDecl parse(Scanner s) {
-        enterParser("while-statm");
-        //ProcDecl stm = new ProcDecl(s.curLineNum());
+        enterParser("proc-decl");
 
-        leaveParser("while-statm");
+        ProcDecl pd = new ProcDecl(s.curToken.id, s.curLineNum());
+
+        s.skip(TokenKind.procedureToken);
+        pd.name = s.curToken.id;
+        s.skip(TokenKind.nameToken);
+
+        if (s.curToken.kind == TokenKind.leftParToken){
+            pd.pdl = ParamDeclList.parse(s);
+        }
+
+        s.skip(TokenKind.semicolonToken);
+
+        pd.block = Block.parse(s);
+
+        s.skip(TokenKind.semicolonToken);
+
+
+        leaveParser("proc-decl");
         return null;
     }
 
