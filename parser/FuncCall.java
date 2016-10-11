b5bc9513e232723541c28adeda9a84d0255522b1
silmer <dahl.silmer@gmail.com>
2016-10-11 14:22:28 +0200

    parse og prettyprint i Func call

--- parser/FuncCall.java
+++ parser/FuncCall.java
@@ -1,11 +1,19 @@
 package parser;
 
+import main.Main;
 import scanner.Scanner;
+
+import java.util.ArrayList;
+
+import static scanner.TokenKind.*;
 
 /**
  * Created by marius on 22.09.2016.
  */
 class FuncCall extends Factor {
+
+    private String name;
+    private ArrayList<Expression> eList = new ArrayList<Expression>();
 
     FuncCall(int lNum) {
         super(lNum);
@@ -13,20 +21,51 @@ class FuncCall extends Factor {
 
     @Override
     public String identify() {
-        return "<NAVN-HER> on line " + lineNum;
+        return "<func call> on line " + lineNum;
     }
 
     @Override
     void prettyPrint() {
+        Main.log.prettyPrint(name);
+
+        if (!eList.isEmpty()){
+            Main.log.prettyPrint("(");
+            eList.get(0).prettyPrint();
+
+            for (int i = 1; i < eList.size(); i++){
+                Main.log.prettyPrint(",");
+                eList.get(i).prettyPrint();
+            }
+            Main.log.prettyPrint(")");
+        }
 
     }
 
     static FuncCall parse(Scanner s) {
         enterParser("while-statm");
 
-        FuncCall stm = new FuncCall(s.curLineNum());
+        FuncCall fc = new FuncCall(s.curLineNum());
+
+        s.test(nameToken);
+        fc.name = s.curToken.id;
+        s.skip(nameToken);
+
+        if (s.curToken.kind == leftParToken){
+            s.skip(leftParToken);
+
+            fc.eList.add(Expression.parse(s));
+
+            while (s.curToken.kind == commaToken){
+                s.skip(commaToken);
+                fc.eList.add(Expression.parse(s));
+            }
+
+            s.skip(rightParToken);
+
+        }
+
 
         leaveParser("while-statm");
-        return stm;
+        return fc;
     }
 }
