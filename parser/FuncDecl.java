47b70fe2aac72993cc3b8cef879f5123f1ee45eb
Marius Christensen <marius93@gmail.com>
2016-10-13 12:47:27 +0200

    fikset FuncDecl

--- parser/FuncDecl.java
+++ parser/FuncDecl.java
@@ -1,11 +1,19 @@
 package parser;
 
+import main.Main;
 import scanner.Scanner;
+import static scanner.TokenKind.*;
+
 
 /**
  * Created by marius on 22.09.2016.
  */
 class FuncDecl extends ProcDecl {
+
+    private String name;
+    private ParamDeclList pdl;
+    private parser.TypeName tn;
+    private Block blck;
 
     FuncDecl(String id, int lNum) {
         super(id, lNum);
@@ -13,20 +21,43 @@ class FuncDecl extends ProcDecl {
 
     @Override
     public String identify() {
-        return "<NAVN-HER> on line " + lineNum;
+        return "<func-decl> on line " + lineNum;
     }
 
     @Override
     void prettyPrint() {
+        Main.log.prettyPrint("function");
+        Main.log.prettyPrint(name);
 
+        if (pdl != null)
+            pdl.prettyPrint();
+
+        Main.log.prettyPrint(":");
+        tn.prettyPrint();
+        Main.log.prettyPrint(";");
+        blck.prettyPrint();
+        Main.log.prettyPrint(";");
     }
 
     static FuncDecl parse(Scanner s) {
-        enterParser("while-statm");
+        enterParser("func-decl");
+        FuncDecl fd = new FuncDecl(s.curToken.id, s.curLineNum());
 
-        //FuncDecl stm = new FuncDecl(s.curLineNum());
+        s.skip(functionToken);
+        s.test(nameToken);
+        fd.name = s.curToken.id;
+        s.skip(nameToken);
 
-        leaveParser("while-statm");
-        return null;
+        if (s.curToken.kind == leftParToken)
+            fd.pdl = ParamDeclList.parse(s);
+
+        s.skip(colonToken);
+        fd.tn = TypeName.parse(s);
+        s.skip(semicolonToken);
+        fd.blck = Block.parse(s);
+        s.skip(semicolonToken);
+
+        leaveParser("func-decl");
+        return fd;
     }
 }
